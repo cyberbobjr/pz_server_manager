ee2f65c04145893aa377ab59805cd6ed53cf7a4d
Benjamin MARCHAND <cyberbobjr@gmail.com>
2023-01-23 07:52:33 +0100

    WIP

--- back/libs/token_required.py
+++ back/libs/token_required.py
@@ -1,9 +1,6 @@
-import json
-from functools import wraps
-
+import datetime
 import jwt
-from flask import jsonify, request
-
+from functools import wraps
 from back.libs import Config
 
 
@@ -11,20 +8,23 @@ def token_required(func):
     @wraps(func)
     def decorator(*args, **kwargs):
         token = None
+        message = jsonify({'message': 'a valid token is missing'})
         if 'Authorization' in request.headers:
             token = request.headers['Authorization'].replace("Bearer ", "")
 
         if not token:
-            message = jsonify({'message': 'a valid token is missing'})
-            return message, 403
+            return message, 401
+
         secret = Config.app_config['auth']['secret']
         try:
             data = jwt.decode(token, secret, algorithms="HS256")
-            current_user = {'username': 'toto'}
+            expireIn = datetime.datetime.fromtimestamp(int(data["expireIn"]))
+            if expireIn < datetime.datetime.now():
+                return message, 401
+            current_user = {'username': data["username"]}
         except Exception as e:
-            secret = Config.app_config['auth']['secret']
-            good_token = jwt.encode({'username': 'toto'}, secret, algorithm='HS256')
-            return jsonify({'message': good_token})
-        return func(current_user, *args, **kwargs)
+            message = jsonify({'message': 'a valid token is missing'})
+            return message, 401
+        return func(*args, **kwargs)
 
     return decorator
--- back/routes/auth.py
+++ back/routes/auth.py
@@ -1,7 +1,29 @@
-from back.libs.token_required import token_required
+import datetime
+import jwt
+from fastapi import APIRouter, Body, Depends, HTTPException
+from pydantic import BaseModel
+
+from libs import Config
+
+router = APIRouter()
 
 
-@token_required
-def test(p):
-    print("ok")
-    return "ok"
+class Token(BaseModel):
+    token: str
+
+
+@router.post("/auth/login", tags=["auth"])
+def login(username: str = Body(), password: str = Body()) -> Token:
+    message = 'Incorrect credentials'
+    try:
+        if username == Config.app_config["auth"]["username"] and password == Config.app_config["auth"]["password"]:
+            secret = Config.app_config['auth']['secret']
+            payload = {
+                'username': username,
+                'expireIn': (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%s")
+            }
+            return Token(token=jwt.encode(payload, secret, algorithm='HS256'))
+        raise HTTPException(status_code=401, detail=message)
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=401, detail=message)
