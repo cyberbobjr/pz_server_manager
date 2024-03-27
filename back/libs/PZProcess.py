4058c34e2517783af63df235fe4e6d775ba75d33
cyberbobjr <cyberbobjr@yahoo.com>
2024-03-27 16:45:45 +0100

    add log info

--- back/libs/PZProcess.py
+++ back/libs/PZProcess.py
@@ -1,4 +1,8 @@
+import logging
 import psutil
+
+# Configuration du logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class PZProcess:
@@ -6,30 +10,37 @@ class PZProcess:
         self.pz_exe_path = pz_exe_path
 
     def get_process(self):
-        for process in psutil.process_iter(['pid', 'name']):
+        logging.debug("Recherche du processus de Project Zomboid en cours...")
+        for process in psutil.process_iter(['pid', 'name', 'cmdline']):
             try:
-                psutil.Process(process.pid)
                 arguments = psutil.Process(process.pid).cmdline()
                 if process.name() == "java.exe" and 'zombie.network.GameServer' in arguments:
+                    logging.debug(f"Processus trouvé : {process.name()}")
                     return process
                 if process.name() == "cmd.exe" and f'{self.pz_exe_path}\\StartServer64.bat' in arguments:
+                    logging.debug(f"Processus trouvé : {process.name()}")
                     return process
                 if process.name() == "ProjectZomboid64":
+                    logging.debug(f"Processus trouvé : {process.name()}")
                     return process
                 if process.name() == "ProjectZomboid32":
+                    logging.debug(f"Processus trouvé : {process.name()}")
                     return process
-            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-                pass
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess) as e:
+                logging.error(f"Erreur lors de l'accès au processus {process.pid}: {e}")
+        logging.debug("Aucun processus de Project Zomboid correspondant trouvé.")
         return None
 
     def get_pid(self):
         process = self.get_process()
         if process is not None:
             return process.pid
+        logging.debug("Aucun PID trouvé pour un processus de Project Zomboid.")
         return None
 
     def get_running_time(self):
         process = self.get_process()
         if process is None:
+            logging.debug("Impossible de récupérer le temps d'exécution : aucun processus de Project Zomboid trouvé.")
             return None
         return process.create_time()
