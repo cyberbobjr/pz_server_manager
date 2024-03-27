5dbe7bdca1f5f8addf8e6c177e167ae5195e4ac7
cyberbobjr <cyberbobjr@yahoo.com>
2024-03-27 15:52:21 +0100

    change starting script

--- start.sh
+++ start.sh
@@ -3,11 +3,18 @@
 # Absolute path to the script directory
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
+# Check if pidfile exists and stop the previous process
+if [ -f "${SCRIPT_DIR}/back/pidfile" ]; then
+    PID=$(cat "${SCRIPT_DIR}/back/pidfile")
+    kill "$PID"
+    rm "${SCRIPT_DIR}/back/pidfile"
+fi
+
 # Activate the Python virtual environment
 source "${SCRIPT_DIR}/back/pz_python/bin/activate"
 
 # Change working directory
 cd "${SCRIPT_DIR}/back" || { echo "The 'back' directory was not found."; exit 1; }
 
-# Execute the Python script
+# Start the application
 python3 main.py & echo $! > "${SCRIPT_DIR}/back/pidfile"
