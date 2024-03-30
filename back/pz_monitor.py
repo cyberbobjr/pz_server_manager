479e00a80561abd0ae7eceaf539e8490f24e93bb
Benjamin MARCHAND <cyberbobjr@gmail.com>
2024-03-30 11:46:10 +0100

    fix startup script for monitoring

--- back/pz_monitor.py
+++ back/pz_monitor.py
@@ -6,21 +6,8 @@ from libs.DatetimeHelper import DatetimeHelper
 from libs.PZLog import PZLog
 from pz_setup import pzGame, steam, pzRcon
 
+# Configuration du logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-ongoing_tasks = []
-
-
-def signal_handler(sig, frame):
-    logging.info("Signal received, initiating graceful shutdown...")
-    for task in ongoing_tasks:
-        task.cancel()
-    asyncio.get_event_loop().run_until_complete(asyncio.gather(*ongoing_tasks))
-    logging.info("All tasks completed, exiting.")
-    exit(0)
-
-
-signal.signal(signal.SIGINT, signal_handler)
-signal.signal(signal.SIGTERM, signal_handler)
 
 
 async def monitor_mod_update():
@@ -45,12 +32,28 @@ async def monitor_mod_update():
                         await pzGame.stop_server()
                         break
                 except Exception as e:
-                    print(f'{e}')
+                    logging.error(f'Error checking mod update for {workshop_id}: {e}')
                     continue
-        await asyncio.sleep(30 * 60)  # check every demihour
+        await asyncio.sleep(30 * 60)  # Check every half-hour
+
+
+def signal_handler(sig, frame):
+    logging.info("Signal received, initiating graceful shutdown...")
+    tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
+    [task.cancel() for task in tasks]
+    logging.info("Cancelled ongoing tasks. Exiting.")
+    asyncio.get_event_loop().stop()
+
+
+def main():
+    # Configure signal handling
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    # Start the monitor task
+    logging.info("Startup: Launching mods monitor...")
+    asyncio.run(monitor_mod_update())
 
 
 if __name__ == "__main__":
-    print("Startup: Launching mods monitor...")
-    task = asyncio.create_task(monitor_mod_update())
-    ongoing_tasks.append(task)
+    main()
