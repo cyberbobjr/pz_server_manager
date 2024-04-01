21ec89a08abc325e3770b5605d6a42bf6db2790b
Benjamin MARCHAND <cyberbobjr@gmail.com>
2024-04-01 12:26:10 +0200

    fix startup script for monitoring

--- start-bot.sh
+++ start-bot.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 # Activate the Python virtual environment
 source "${SCRIPT_DIR}/back/pz_python/bin/activate"
-
+echo "Launched in ${SCRIPT_DIR}"
 # Change working directory
 cd "${SCRIPT_DIR}/back" || { echo "The 'back/pz_bot' directory was not found."; exit 1; }
 
