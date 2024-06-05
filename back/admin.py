16d44feb8d3bcccc54a9f6f682bc6f71130290fe
Ubuntu <ubuntu@ns3014108.ip-149-202-65.eu>
2024-06-05 08:30:14 +0000

    evol

---
+++ back/admin.py
@@ -0,0 +1,26 @@
+import yaml
+
+from libs.console import Console
+
+# Chemin vers votre fichier config.yml
+CONFIG_FILE_PATH = 'config.yml'
+
+
+def load_rcon_config(config_path):
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+        return config['rcon']['host'], int(config['rcon']['port']), config['rcon']['password']
+
+
+def stop_project_zomboid_server():
+    try:
+        host, port, password = load_rcon_config(CONFIG_FILE_PATH)
+        rcon = Console(host, port=port, password=password)
+        rcon.command("setaccesslevel batman admin")
+        rcon.close()
+    except Exception as e:
+        print("Erreur lors de la tentative de stopper le serveur via RCON:", e)
+
+
+if __name__ == "__main__":
+    stop_project_zomboid_server()
