0c22f24581b18ce29c8ae90fae2c81c55ac36e4a
cyberbobjr <cyberbobjr@yahoo.com>
2024-07-24 17:31:42 +0200

    mise à jour front

--- front/src/app/layouts/full/sidebar/branding.component.ts
+++ front/src/app/layouts/full/sidebar/branding.component.ts
@@ -3,15 +3,15 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-branding',
   template: `
-      <div class="branding">
-          <a href="/">
-              <img src="./assets/images/logos/logo.webp"
-                   style="width: 100%;"
-                   class="align-middle m-2"
-                   alt="logo"
-              />
-          </a>
-      </div>
+    <div class="branding">
+      <a href="/">
+        <img src="./assets/images/logos/logo.webp"
+             style="width: 100%;max-width:150px"
+             class="align-middle m-2"
+             alt="logo"
+        />
+      </a>
+    </div>
   `,
 })
 export class BrandingComponent {
